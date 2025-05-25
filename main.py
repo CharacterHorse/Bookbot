0021ea91790f8bd3cd381acfc6d24756e4423700
CharacterHorse <github@rishay.live>
2025-05-25 17:09:55 +0530

    modularity
    Signed-off-by: CharacterHorse <github@rishay.live>

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_num_letters, get_sorted_letter_dict
 
 
@@ -14,7 +15,10 @@ Found {get_num_words(content)} total words
 
 
 def main():
-    get_book_text(filepath="books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+    get_book_text(filepath=sys.argv[1])
 
 
 main()
